6b50c7d90b322f4584667b4a2422db0a46ec6b93
Denny Azevedo <denny_af@yahoo.com.br>
2026-05-31 20:31:00 -0300

    criando user service e controller de autenticação

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,11 +1,37 @@
 import { RequestHandler } from 'express'
+import { z } from 'zod'
+import { createUser } from '../services/user.service'
 
 export const signIn: RequestHandler = async (req, res) => {
 	// Implementation for user sign-in
 }
 
 export const signUp: RequestHandler = async (req, res) => {
-	// Implementation for user sign-up
+	const schema = z.object({
+		name: z.string(),
+		email: z.string().email(),
+		password: z.string()
+	})
+	const data = schema.safeParse(req.body)
+	if (!data.success) {
+		return res.status(400).json({ error: data.error.flatten().fieldErrors })
+	}
+
+	const newUser = await createUser(data.data)
+	if (!newUser) {
+		return res.status(400).json({ error: 'Email already in use' })
+	}
+
+	const token = '123'
+	res.status(201).json({
+		message: 'User created successfully',
+		user: {
+			id: newUser.id,
+			name: newUser.name,
+			email: newUser.email
+		},
+		token
+	})
 }
 
 export const validate: RequestHandler = async (req, res) => {
---
+++ src/services/user.service.ts
@@ -0,0 +1,27 @@
+import { prisma } from '../libs/prisma'
+import bcrypt from 'bcryptjs'
+
+type CreateUserProps = {
+	name: string;
+	email: string;
+	password: string;
+}
+
+export const createUser = async ({ name, email, password }: CreateUserProps) => {
+	email = email.toLowerCase()
+
+	const user = await prisma.user.findFirst({
+		where: { email }
+	})
+	if (user) return false
+
+	const passwordHash = await bcrypt.hashSync(password, 10)
+
+	return await prisma.user.create({
+		data: {
+			name,
+			email,
+			password: passwordHash
+		}
+	})
+}
