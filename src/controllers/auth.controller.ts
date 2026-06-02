e906cdfc3dd7c3a842ec72e77656fe0b12a0bca9
Denny Azevedo <denny_af@yahoo.com.br>
2026-06-02 20:52:53 -0300

    criateToken function for generating JWT tokens based on user information. Implemented signIn and signUp controllers for handling user authentication, including input validation and token generation. Added verifyUser function to check user credentials during sign-in.

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,9 +1,33 @@
 import { RequestHandler } from 'express'
 import { z } from 'zod'
-import { createUser } from '../services/user.service'
+import { createUser, verifyUser } from '../services/user.service'
+import { createToken } from '../services/auth.service'
 
 export const signIn: RequestHandler = async (req, res) => {
-	// Implementation for user sign-in
+	const schema = z.object({
+		email: z.string().email(),
+		password: z.string()
+	})
+	const data = schema.safeParse(req.body)
+	if (!data.success) {
+		return res.status(400).json({ error: data.error.flatten().fieldErrors })
+	}
+
+	const user = await verifyUser(data.data)
+	if (!user) {
+		return res.status(400).json({ error: 'Invalid email or password' })
+	}
+
+	const token = createToken(user)
+	res.json({
+		message: 'User signed in successfully',
+		user: {
+			id: user.id,
+			name: user.name,
+			email: user.email
+		},
+		token
+	})
 }
 
 export const signUp: RequestHandler = async (req, res) => {
@@ -22,7 +46,7 @@ export const signUp: RequestHandler = async (req, res) => {
 		return res.status(400).json({ error: 'Email already in use' })
 	}
 
-	const token = '123'
+	const token = createToken(newUser)
 	res.status(201).json({
 		message: 'User created successfully',
 		user: {
