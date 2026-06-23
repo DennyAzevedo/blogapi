643920b0da47cf7d38cb6d511b90cb19ccf59347
Denny Azevedo <denny_af@yahoo.com.br>
2026-06-23 16:45:46 -0300

    create post management with cover upload and slug generation

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -1,21 +1,72 @@
-import { RequestHandler } from 'express'
+import { RequestHandler, Response } from 'express'
+import { ExtendedRequest } from '../types/extended-request'
+import z from 'zod'
+import { title } from 'node:process'
+import { createPost, createPostSlug, handleCover } from '../services/post.service'
+import { getUserById } from '../services/user.service'
+import { create } from 'node:domain'
+import { coverToUrl } from '../utils/cover-to-url';
 
-export const addPost: RequestHandler = async (req, res) => {
-	// Implementation for adding a new post
+export const addPost = async (req: ExtendedRequest, res: Response) => {
+	if (!req.user) return
+	
+	const schema = z.object({
+		title: z.string(),
+		tags: z.string(),
+		body: z.string()
+	})
+	const data = schema.safeParse(req.body)
+	if (!data.success) {
+		return res.status(400).json({ error: data.error.flatten().fieldErrors })
+	}
+
+	if(!req.file) {
+		return res.status(400).json({ error: 'Cover image is required' })
+	}
+	
+	const coverName = await handleCover(req.file)
+	if(!coverName) {
+		return res.status(400).json({ error: 'Invalid cover image' })
+	}
+
+	const slug = await createPostSlug(data.data.title)
+
+	const newPost = await createPost({
+		authorId: req.user.id,
+		slug,
+		title: data.data.title,
+		tags: data.data.tags,
+		body: data.data.body,
+		cover: coverName
+	})
+
+	const author = await getUserById(newPost.authorId)
+
+	res.status(201).json({
+		post: {
+			id: newPost.id,
+			slug: newPost.slug,
+			title: newPost.title,
+			createdAt: newPost.createdAt,
+			cover: coverToUrl(newPost.cover),
+			tags: newPost.tags,
+			authorName: author?.name || 'Unknown'
+		}
+	})
 }
 
-export const editPost: RequestHandler = async (req, res) => {
+export const editPost = async (req: ExtendedRequest, res: Response) => {
 	// Implementation for editing an existing post
 }
 
-export const deletePost: RequestHandler = async (req, res) => {
+export const deletePost = async (req: ExtendedRequest, res: Response) => {
 	// Implementation for deleting a post
 }
 
-export const getAllPosts: RequestHandler = async (req, res) => {
+export const getAllPosts = async (req: ExtendedRequest, res: Response) => {
 	// Implementation for getting all posts (admin view)
 }
 
-export const getPost: RequestHandler = async (req, res) => {
+export const getPost = async (req: ExtendedRequest, res: Response) => {
 	// Implementation for getting a single post (admin view)
 }
